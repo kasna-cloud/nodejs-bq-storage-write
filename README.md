6a7ed3f5ccd0a3a696363760b3d31c6896b87a0f
Troy Bebee <troy.bebee@kasna.com.au>
2021-11-22 11:03:56 +1100

    Added examples.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# trafficguard
+NodeJS Client for BigQuery Storage API
+=====
 
+# Example 1
+https://stackoverflow.com/questions/69793756/write-rows-to-bigquery-via-nodejs-bigquery-storage-write-api
+
+
+# Example 2
+With a static-ish schema, the "raw" API should be feasible.
+
+You'll need to create a proto2 schema and compile it to Node.js, similar to what is done in the Python sample(s): https://github.com/googleapis/python-bigquery-storage/blob/main/samples/snippets/sample_data.proto Be mindful of the restrictions documented here: https://cloud.google.com/bigquery/docs/write-api#use_the_grpc_api_directly importantly, "The protocol buffer message cannot use a package specifier, and all nested or enumeration types must be defined within the top-level root message. References to external messages are not allowed"
+
+Using the Node.js client should be similar to how we use it in Python https://github.com/googleapis/python-bigquery-storage/blob/main/samples/snippets/append_rows_proto2.py
+
+0. Create a BigQueryWriteClient https://cloud.google.com/nodejs/docs/reference/bigquery-storage/latest/bigquery-storage/v1.bigquerywriteclient
+1. Create a stream (or use the "default" stream for a table) https://cloud.google.com/nodejs/docs/reference/bigquery-storage/latest/bigquery-storage/v1.bigquerywriteclient#_google_cloud_bigquery_storage_v1_BigQueryWriteClient_createWriteStream_member_1_
+2. Call appendRows to connect to the stream https://cloud.google.com/nodejs/docs/reference/bigquery-storage/latest/bigquery-storage/v1.bigquerywriteclient#_google_cloud_bigquery_storage_v1_BigQueryWriteClient_appendRows_member_1_
+3. Write batches of rows to the Node.js stream object returned by appendRows
+4. If PENDING mode, finalized the stream https://cloud.google.com/nodejs/docs/reference/bigquery-storage/latest/bigquery-storage/v1.bigquerywriteclient#_google_cloud_bigquery_storage_v1_BigQueryWriteClient_finalizeWriteStream_member_1_
+5. Commit the stream https://cloud.google.com/nodejs/docs/reference/bigquery-storage/latest/bigquery-storage/v1.bigquerywriteclient#_google_cloud_bigquery_storage_v1_BigQueryWriteClient_batchCommitWriteStreams_member_1_
---
+++ client.js
@@ -0,0 +1,98 @@
+const {BigQueryWriteClient} = require('@google-cloud/bigquery-storage').v1;
+const st = require('./sample_data.js')
+const type = require('@google-cloud/bigquery-storage').protos.google.protobuf.FieldDescriptorProto.Type
+const mode = require('@google-cloud/bigquery-storage').protos.google.cloud.bigquery.storage.v1.WriteStream.Type
+
+const storageClient = new BigQueryWriteClient();
+
+const parent = `projects/${project}/datasets/${dataset}/tables/SampleData`
+var writeStream = {type: mode.PENDING}
+
+var sample_data = new st.SampleData()
+    
+var protoDescriptor = {} 
+protoDescriptor.name = 'sample_data'
+protoDescriptor.field = [
+    {'name':'request',
+        'number':1,
+        'type':type.TYPE_INT64},
+    {'name':'url',
+        'number':2,
+        'type':type.TYPE_STRING},
+    {'name':'token',
+        'number':3,
+        'type':type.TYPE_INT64}
+]
+
+async function run() {
+    
+    try {
+    
+        var request = {
+          parent,
+          writeStream
+        }
+        var response = await storageClient.createWriteStream(request);
+    
+        writeStream = response[0].name
+        
+        var serializedRows = []
+
+        //Row 1
+        sample_data.setId(1)
+        sample_data.setName('st1')
+        sample_data.setAge(15)
+
+        serializedRows.push(sample_data.serializeBinary())
+
+        //Row 2
+        sample_data.setId(2)
+        sample_data.setName('st2')
+        sample_data.setAge(15)
+        
+        serializedRows.push(sample_data.serializeBinary())
+
+        var protoRows = {
+            serializedRows
+        }
+        var proto_data = {
+            writerSchema: {protoDescriptor},
+            rows: protoRows
+        }
+
+        // Construct request
+        request = {
+          writeStream,
+          protoRows: proto_data
+        };
+        
+        // Insert rows
+        const stream = await storageClient.appendRows();
+        
+        stream.on('data', response => {
+          console.log(response);
+        });
+        stream.on('error', err => {
+          throw err;
+        });
+        stream.on('end', async () => {
+          /* API call completed */
+          try {
+            var response = await storageClient.finalizeWriteStream({name: writeStream})
+
+            response = await storageClient.batchCommitWriteStreams({parent,writeStreams: [writeStream]})
+          }
+          catch(err) {
+              console.log(err)
+          }
+        });
+        stream.write(request);
+        stream.end();
+    }
+    catch(err) {
+        console.log(err)
+    }
+
+}
+
+run();
